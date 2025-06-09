7d92e2f8917c72eab5c9d94c3885ed498a364e01
Sachin <bisht18sachin@gmail.com>
2025-06-09 17:13:15 +0530

    base url

--- frontend/services/axiosClient.js
+++ frontend/services/axiosClient.js
@@ -1,8 +1,16 @@
 // services/axiosClient.js
 import axios from "axios";
+let baseURL;
 
+if (process.env.NODE_ENV === 'production') {
+
+  baseURL = process.env.REACT_APP_API_PROD_URL
+} else {
+  // In development, use the one from .env.local
+  baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'; // Fallback
+}
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
